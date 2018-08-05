5d72a8749c1893980ef4506db29985a26181ccf5
Akiva Ranells <akranells@gmail.com>
2018-08-05 21:04:04 +0300

    working version with render

--- main.js
+++ main.js
@@ -2,33 +2,51 @@ const POSTS = [];
 
 let uniqueId = 0;
 
-const getInput = function (){
+const getInput = function () {
     return $('#post-name').val();
 };
 
-const createPost = function(){
-    return {text: getInput(), id: uniqueId++};
+const createPost = function () {
+    return { text: getInput(), id: uniqueId++ };
 };
 
-const putInDiv = function(post){
-    $('.posts').append('<p class="post" data-id='+post.id+'><button type="button" class="remove">REMOVE</button>'+post.text+'</p>');
+const putInDiv = function (post) {
+    $('.posts').append('<p class="post" data-id=' + post.id + '><button type="button" class="remove">REMOVE</button>' + post.text + '</p>');
 };
 
-$('button.add-post').on('click', function(){
+const addPost = function () {
+    getInput();
     let toPost = createPost();
     POSTS.push(toPost);
-    putInDiv(toPost);
+}
+
+$('button.add-post').on('click', function () {
+    addPost();
+    renderPosts();
 });
 
-$('div.posts').on('click', 'button.remove', function(){
+$('div.posts').on('click', 'button.remove', function () {
     let getId = $(this).closest('p').data().id;
-    removeFromArray(getId);
-    $(this).closest('p').remove();
+    renderDeletePost(getId);
 });
 
-const removeFromArray = function(gotId){
-    for(let i = 0; i < POSTS.length; i ++){
-        if(POSTS[i].id===gotId){
+const renderPosts = function () {
+    $("div.posts").empty();
+    for(let i = 0 ; i < POSTS.length ; i++){
+        putInDiv(POSTS[i]);
+    }
+    
+};
+
+
+const renderDeletePost = function (gotId) {
+    removeFromArray(gotId);
+    $('[data-id=' + gotId + ']').remove();
+};
+
+const removeFromArray = function (gotId) {
+    for (let i = 0; i < POSTS.length; i++) {
+        if (POSTS[i].id === gotId) {
             POSTS.splice(i, 1);
             break;
         }
